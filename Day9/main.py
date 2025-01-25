5e0f8c6fa1f57f0a0e06a0e9794cbc0458173346
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-01-25 07:09:58 +0530

    Add files via upload

---
+++ Day9/main.py
@@ -0,0 +1,39 @@
+# TODO-1: Ask the user for input
+# dick = {}
+# while True:
+#     user = input("Do you want to Continue: yes or no")
+#     if user=='yes':
+#         name=input("Enter a Bider name:\n")
+#         bid= int(input("Enter the Bid"))
+# # TODO-2: Save data into dictionary {name: price}
+#
+#         dick[name]=bid
+# # TODO-3: Whether if new bids need to be added
+#
+#     else:
+#         break
+# # TODO-4: Compare bids in dictionary
+#
+# print(dick)
+import art
+print(art.logo)
+dick = {}
+def auction(names,bider):
+    dick[name] = bider
+while True:
+    name = input("")
+    bid = int(input(""))
+    user = input("yes or no")
+    auction(names=name,bider=bid)
+    if user == 'no':
+        winner = ''
+        highest_bid = 0
+        for bides in dick:
+            bid_amnt = dick[bides]
+            if bid_amnt>=highest_bid:
+                highest_bid=bid_amnt
+                winner = bides
+        print(f"the maximum bid is {winner}")
+        break
+print(dick)
+
