66c58acb4c172a0d7a0a5cbf4ee2c7e07f3fcf88
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-01-28 18:39:42 +0530

    Update main.py

--- Day9/main.py
+++ Day9/main.py
@@ -1,5 +1,5 @@
 # TODO-1: Ask the user for input
-# dick = {}
+# dict = {}
 # while True:
 #     user = input("Do you want to Continue: yes or no")
 #     if user=='yes':
@@ -7,14 +7,14 @@
 #         bid= int(input("Enter the Bid"))
 # # TODO-2: Save data into dictionary {name: price}
 #
-#         dick[name]=bid
+#         dict[name]=bid
 # # TODO-3: Whether if new bids need to be added
 #
 #     else:
 #         break
 # # TODO-4: Compare bids in dictionary
 #
-# print(dick)
+# print(dict)
 import art
 print(art.logo)
 dict = {}
