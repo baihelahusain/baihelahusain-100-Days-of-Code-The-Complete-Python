33deb445744c85c453980fb84e62ce9481b67462
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-01-30 23:52:00 +0530

    Add files via upload
    debugging day

--- Day13/task.py
+++ Day13/task.py
@@ -1,5 +1,10 @@
-word_per_page = 0
-pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
-total_words = pages * word_per_page
-print(total_words)
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
